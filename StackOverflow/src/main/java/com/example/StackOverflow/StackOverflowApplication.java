c91eac4a199a3b8d86bcdafae56862c78e8e93b8
Barbu Dobocan <barbu_dobocan98@yahoo.com>
2021-10-10 18:09:00 +0300

    NNNIIINJA

---
+++ StackOverflow/src/main/java/com/example/StackOverflow/StackOverflowApplication.java
@@ -0,0 +1,15 @@
+package com.example.StackOverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackOverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackOverflowApplication.class, args);
+
+		System.out.println("NIIIIINJA");
+	}
+
+}
